da1d5269b8c6031373dd91113260fb4586609013
Muhammad Saiful Islam <muhammad@saiful.web.id>
2021-01-29 11:26:40 +0700

    Filter links if chat comes from HN channel
    Filter links so only HN discussion links will be saved if the chat comes
    from the HN channel.

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -9,6 +9,10 @@ import (
 	xurls "mvdan.cc/xurls/v2"
 )
 
+const (
+	HackerNewsChatID = -1001020923877
+)
+
 type LinkSaver func([]string) error
 
 func NewWebhookHandler(token, webhookUrl string, saveLinks LinkSaver) (*tb.Webhook, error) {
@@ -37,6 +41,11 @@ func NewWebhookHandler(token, webhookUrl string, saveLinks LinkSaver) (*tb.Webho
 		links := getLinks(m)
 		log.Printf("Links found: %v", links)
 
+		if m.OriginalChat != nil && m.OriginalChat.ID == HackerNewsChatID {
+			links = getHNCommentLinks(links)
+			log.Printf("Message from HN channel, links filtered to: %v", links)
+		}
+
 		err := saveLinks(links)
 		if err != nil {
 			b.Reply(m, fmt.Sprintf("Error saving link: %s", err), tb.NoPreview)
@@ -52,4 +61,20 @@ func NewWebhookHandler(token, webhookUrl string, saveLinks LinkSaver) (*tb.Webho
 
 func getLinks(m *tb.Message) []string {
 	return xurls.Relaxed().FindAllString(m.Text, -1)
+}
+
+const hnCommentLinkPrefix = "https://readhacker.news/c/"
+
+func getHNCommentLinks(links []string) []string {
+	for _, link := range links {
+		if len(link) < len(hnCommentLinkPrefix) {
+			continue
+		}
+
+		if link[:len(hnCommentLinkPrefix)] == hnCommentLinkPrefix {
+			return []string{link}
+		}
+	}
+
+	return []string{}
 }
